3cbd91e5cb8e20a22c530916f4573f2d4d1c3f84
t3ddev <t3ddev@gmail.com>
2021-11-10 00:49:44 +0200

    fix bug with freeze storage

--- src/components/TimerWrapper/TimerWrapper.jsx
+++ src/components/TimerWrapper/TimerWrapper.jsx
@@ -1,76 +1,81 @@
-import React, { useState, useEffect } from 'react'
-import { useTimerService } from '../../services/timerService';
-import { Button } from '../Button/Button';
-import { Timer } from '../Timer/Timer';
-import './timerWrapper.css';
-
-
+import React, { useState, useEffect } from "react";
+import { useTimerService } from "../../services/timerService";
+import { Button } from "../Button/Button";
+import { Timer } from "../Timer/Timer";
+import "./timerWrapper.css";
 
 export const TimerWrapper = () => {
+  const [time, setTime] = useState(0);
+  const [timeArray, setTimeArray] = useState([time]);
+
+  const [showStart, setShowStart] = useState(true);
+
+  const [
+    newTimer,
+    resetTimer,
+    stopTimer,
+    syncItUp,
+    startTimer,
+    channel
+  ] = useTimerService(time, setTime, setShowStart, timeArray, setTimeArray);
 
 
-    const [time, setTime] = useState(0);
-    const [showStart, setShowStart] = useState(true)
+  useEffect(() => {
+    setShowStart(localStorage.getItem("stopTick") ? true : false);
+    channel.onmessage = (ev) => {
+      if (ev.data === "reset") {
+        setTimeArray([]);
+        setTime(0);
+        return;
+      }
+      if (ev.data === 'stop') {
+        setShowStart(true)
+      }
+      if (ev.data.timeArr) {
+        setTimeArray(ev.data.timeArr);
+      }
+      if (ev.data.timeArr) {
+        setTimeArray(ev.data.timeArr);
+      }
+      if (ev.data.timeNow) {
+      const timeNow = new Date().getTime();
+      startTimer(1000 - (timeNow - ev.data));
+      }
+    };
 
+    return () => {
+      channel.close();
+    };
+  }, [channel, startTimer, showStart, timeArray]);
 
-    const [newTimer, resetTimer, stopTimer, syncItUp, startTimer, channel, timeArray] = useTimerService(time, setTime, setShowStart);
-
-
-    useEffect(() => {
-        localStorage.setItem('currentTime', JSON.stringify(timeArray))
-    }, [timeArray])
-
-    useEffect(() => {
-        // setTimeArray(JSON.parse(window.localStorage.getItem('currentTime')))
-        setShowStart(localStorage.getItem('stopTick') ? true : false)
-        channel.onmessage = ev => {
-            console.log(ev);
-            if (ev.data === 'reset') {
-                setTime(0)
-                return
-            }
-            const timeNow = new Date().getTime();
-            startTimer(1000 - (timeNow - ev.data));
-        };
-
-        return () => {
-            channel.close();
-        };
-    }, [channel, startTimer, showStart]);
-
-
-    const currentTime = JSON.parse(window.localStorage.getItem('currentTime'))
-
-
-
-    return (
-        <div className="timer-container">
-            <span>Timer</span>
-            <Timer seconds={time} setSeconds={setTime} />
-            {showStart ?
-                <div className="button-container">
-                    <Button value={'Start'} onPress={syncItUp} />
-                    <Button value={'Reset'} onPress={resetTimer} />
-                </div>
-                :
-                <div className="button-container">
-
-                    <Button value={'Stop'} onPress={stopTimer} />
-                    <Button value={'Lap'} onPress={newTimer} />
-                </div>
-            }
-
-            <div className="result-timer">
-                {currentTime && currentTime.map((time, index) => index !== 0 && (
-                    <div key={time + index} className='time-list'>
-                        <p className='time-text'>{`Lap ${index}`}</p>
-                        <p className='time-text'>{time}</p>
-                    </div>
-                )
-                )
-
-                }
-            </div>
+  return (
+    <div className="timer-container">
+      <span>Timer</span>
+      <Timer seconds={time} setSeconds={setTime} />
+      {showStart ? (
+        <div className="button-container">
+          <Button value={"Start"} onPress={syncItUp} />
+          <Button value={"Reset"} onPress={resetTimer} />
         </div>
-    )
-}
+      ) : (
+        <div className="button-container">
+          <Button value={"Stop"} onPress={stopTimer} />
+          <Button value={"Lap"} onPress={newTimer} />
+        </div>
+      )}
+
+      <div className="result-timer">
+        {timeArray &&
+          timeArray.map(
+            (time, index) =>
+              time !== 0 && (
+                <div className="time-list">
+                  <p className="time-text">{`Lap ${index + 1}`}</p>
+                  <p className="time-text">{time}</p>
+                </div>
+              )
+          )}
+      </div>
+    </div>
+  );
+};
--- src/services/timerService.jsx
+++ src/services/timerService.jsx
@@ -1,63 +1,64 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useState } from "react";
 
+export const useTimerService = (
+    time,
+    setTime,
+    setShowStart,
+    timeArray,
+    setTimeArray
+) => {
+    const { localStorage } = window;
 
-export const useTimerService = (time, setTime, setShowStart) => {
+    const channel = new BroadcastChannel("timer");
 
-
-    const [timeArray, setTimeArray] = useState([time])
-
-    const { localStorage } = window
-
-
-    const channel = new BroadcastChannel("timer")
-
-    console.log(JSON.parse(window.localStorage.getItem('currentTime')))
-
-
-    const startTimer = useCallback(delay => {
-
-        const interval = setInterval(() => {
-            if (localStorage.getItem('stopTick')) {
-                channel.close()
-                clearInterval(interval)
-                return
-            }
-            setTime(time => time + 1);
-        }, 1000);
-    }, [channel, setTime]);
-
+    const startTimer = useCallback(
+        (delay) => {
+            const interval = setInterval(() => {
+                if (localStorage.getItem("stopTick")) {
+                    channel.close();
+                    clearInterval(interval);
+                    return;
+                }
+                setTime((time) => time + 1);
+            }, 1000);
+        },
+        [channel, setTime]
+    );
 
     const syncItUp = () => {
         const timeNow = new Date().getTime();
-        localStorage.clear('stopTick')
-        channel.postMessage(timeNow);
+        localStorage.clear("stopTick");
+        channel.postMessage({timeNow: timeNow});
         startTimer(1000);
     };
 
-
     const stopTimer = () => {
-        localStorage.setItem('stopTick', true)
-        setShowStart(true)
-    }
+        localStorage.setItem("stopTick", true);
+        channel.postMessage("stop");
+        setShowStart(true);
+    };
 
     const resetTimer = () => {
-        setTime(0)
-        setTimeArray([])
-        localStorage.clear('currentTime')
-        channel.postMessage('reset')
-        stopTimer()
-    }
+        setTime(0);
+        setTimeArray([]);
+        channel.postMessage("reset");
+        stopTimer();
+    };
 
     const newTimer = () => {
-        const storageTimeArr = JSON.parse(window.localStorage.getItem('currentTime'))
-        console.log(storageTimeArr)
-        console.log(timeArray);
-        
-        setTimeArray([...timeArray, time])
-        console.log(timeArray)
-    }
+        const timeArr = { timeArr: [...timeArray, time] }
+        channel.postMessage(timeArr);
 
-    return [newTimer, resetTimer, stopTimer, syncItUp, startTimer, channel, timeArray]
+        setTimeArray([...timeArray, time]);
+    };
 
-
-}
+    return [
+        newTimer,
+        resetTimer,
+        stopTimer,
+        syncItUp,
+        startTimer,
+        channel,
+        timeArray
+    ];
+};
